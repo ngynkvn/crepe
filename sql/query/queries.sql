75e8c02fa25b996bd16c92e1f3b566c469fbdeae
Kevin Nguyen <ngynkvn@gmail.com>
2024-08-20 19:18:33 -0400

    feat: Add sqlc codegen

---
+++ sql/query/queries.sql
@@ -0,0 +1,25 @@
+-- name: AddRepo :one
+INSERT INTO code_repositories (
+  repo, repo_type
+) VALUES (
+  $1, $2
+)
+RETURNING *;
+
+
+-- name: GetFileByName :one
+SELECT * 
+FROM code_files
+WHERE file_name = $1 LIMIT 1;
+
+-- name: AddFile :one
+INSERT INTO code_files (
+  repo_id, file_path, file_name, programming_language, contents
+) SELECT repo_id
+       , $2
+       , $3
+       , $4
+       , $5
+  FROM code_repositories
+  WHERE repo=$1
+RETURNING *;
